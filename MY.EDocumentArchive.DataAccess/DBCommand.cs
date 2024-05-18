723c492d6c5bd69cfe36626ca7af3b2dc0cbe8fd
Sadjad Refagat <sadjadrefagat@gmail.com>
2024-05-18 12:19:13 +0330

    1

---
+++ MY.EDocumentArchive.DataAccess/DBCommand.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace MY.EDocumentArchive.DataAccess
+{
+    static public class DBCommand
+    {
+        static public List<T> GetList<T>() where T : class
+        {
+            using (var connection = new SqlConnection(""))
+            {
+                var cd = new CommandDefinition();
+                cd.
+                connection.Open();
+                connection.Query<T>(cd).ToList();
+                connection.Close();
+            }
+
+            return null;
+        }
+    }
+}
